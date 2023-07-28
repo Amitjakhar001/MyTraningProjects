8b56e43cf5f15389a272e26741a3a30ac40db667
JBAnkitjakhar <ankitjakharabc@gmail.com>
2023-07-28 21:55:13 +0530

    Initial Commit

---
+++ zomatoReact/client/src/components/Policy.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Policy = () => {
+  return (
+    <>
+      <Container style={{ marginTop: "50px" }}>
+        <h1>Terms and policy</h1>
+        <Row>
+          <Col md={10}>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              praesentium?
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam
+              officia nemo unde neque iusto minima reiciendis vitae nam deserunt
+              qui nulla rem voluptas, non velit earum, corrupti delectus sint!
+              Quibusdam quam aliquam eveniet molestias fuga assumenda culpa at,
+          
+            </p>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              praesentium?
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam
+              officia nemo unde neque iusto minima reiciendis vitae nam deserunt
+              qui nulla rem voluptas, non velit earum, corrupti delectus sint!
+              Quibusdam quam aliquam eveniet molestias fuga assumenda culpa at,
+              
+            </p>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              praesentium?
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam
+              officia nemo unde neque iusto minima reiciendis vitae nam deserunt
+              qui nulla rem voluptas, non velit earum, corrupti delectus sint!
+              Quibusdam quam aliquam eveniet molestias fuga assumenda culpa at,
+            
+            </p>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              praesentium?
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam
+              officia nemo unde neque iusto minima reiciendis vitae nam deserunt
+              qui nulla rem voluptas, non velit earum, corrupti delectus sint!
+              Quibusdam quam aliquam eveniet molestias fuga assumenda culpa at,
+        
+              voluptatem ipsa, minus magni, quisquam illum sit quis itaque quas.
+            </p>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              praesentium?
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam
+              officia nemo unde neque iusto minima reiciendis vitae nam deserunt
+              qui nulla rem voluptas, non velit earum, corrupti delectus sint!
+              Quibusdam quam aliquam eveniet molestias fuga assumenda culpa at,
+              temporibus tempore
+            </p>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              praesentium?
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In magnam
+              officia nemo unde neque iusto minima reiciendis vitae nam deserunt
+              qui nulla rem voluptas, non velit earum, corrupti delectus sint!
+              Quibusdam quam aliquam eveniet molestias fuga assumenda culpa at,
+              temporibus tempore perspiciatis repellat architecto iste
+              aspernatur 
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Policy;
